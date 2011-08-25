e3f9fff8d2bbd60e5236a7f148bfac978dd9fa78
Takayuki YAMAGUCHI <d@ytak.info>
2011-08-25 10:56:37 +0900

    Delete ruby_mpfr.h etc. because we use the header in ruby-mpfr gem.

--- ext/mpfi/extconf.rb
+++ ext/mpfi/extconf.rb
@@ -20,5 +20,16 @@ dir_config("mpfr")
 dir_config("mpfi")
 dir_config("gmp")
 if have_header('mpfr.h') && have_library('mpfr') && have_header('mpfi.h') && have_library('mpfi') && have_header('gmp.h') && have_library('gmp')
+
+  ruby_mpfr_header_dir = nil
+  begin
+    require "rubygems"
+    spec = Gem::Specification.find_by_name("ruby-mpfr")
+    ruby_mpfr_header_dir = File.join(spec.full_gem_path, 'ext/mpfr')
+  rescue LoadError
+  end
+  unless find_header("ruby_mpfr.h", ruby_mpfr_header_dir)
+    header_not_found("ruby_mpfr.h")
+  end
   create_makefile("mpfi")
 end
--- ext/mpfi_matrix/mpfi/extconf.rb
+++ ext/mpfi_matrix/mpfi/extconf.rb
@@ -19,5 +19,19 @@ dir_config('mpfi')
 dir_config('gmp')
 
 if have_header('mpfr.h') && have_library('mpfr') && have_header('mpfi.h') && have_library('mpfi') && have_header('gmp.h') && have_library('gmp')
+  ruby_mpfr_dir = nil
+  begin
+    require "rubygems"
+    spec = Gem::Specification.find_by_name("ruby-mpfr")
+    ruby_mpfr_dir = File.join(spec.full_gem_path, 'ext')
+  rescue LoadError
+  end
+  unless find_header("ruby_mpfr.h", File.join(ruby_mpfr_dir, 'mpfr'))
+    header_not_found("ruby_mpfr.h")
+  end
+  unless find_header("ruby_mpfr_matrix.h", File.join(ruby_mpfr_dir, 'mpfr_matrix/mpfr'))
+    header_not_found("ruby_mpfr_matrix.h")
+  end
+  find_header("ruby_mpfi.h", File.join(File.dirname(__FILE__), "../../mpfi"))
   create_makefile("mpfi/matrix")
 end
